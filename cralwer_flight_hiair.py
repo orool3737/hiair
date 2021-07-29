8a4e5321af67d181e7dd91c460cbb7b6fbc6ad69
orool3737 <orool3737@naver.com>
2021-07-29 15:48:05 +0900

    Update cralwer_flight_hiair.py

--- cralwer_flight_hiair.py
+++ cralwer_flight_hiair.py
@@ -78,9 +78,7 @@ def exract_flight_hiair(flight_day, flight_month):
    while soup:
        try:
            department_hour = datetime.datetime.strptime(department_time[3*i].text, '%H:%M').hour
-         '''
-           if department_hour >= 18 and department_hour < 19 and int(price[i].text.replace(',', '')) <= 50000 :
-           '''
+           #if department_hour >= 18 and department_hour < 19 and int(price[i].text.replace(',', '')) <= 50000 :
            content.append(company[i].text + " " + department_time[3*i].text + " ￦" + price[i].text)
            i = i + 1
        except IndexError:
